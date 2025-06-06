f60e0b2d9bc66051a6d0c0c91a69629872f7d446
Chima Chinagorom <basemaker034@gmail.com>
2025-06-06 04:11:52 -0700

    ReadMe.md

--- index.js
+++ index.js
@@ -1,2 +1,34 @@
-let input = window.prompt("type your name")
-console.log(input)
+var firstName = "Chinagorom"
+var lastName = "jeremiah"
+var age = "21"
+var school = "adventure"
+var graduated = "true"
+
+console.log(`My name is ${firstName} ${lastName} and i am ${age} and i study at ${school} and i graduated: ${true}`)
+
+document.getElementById("firstname").textContent = `Firstname: ${firstName}`
+document.getElementById("lastname").textContent = `Lastname: ${lastName}`
+document.getElementById("school").textContent = `School: ${school}`
+document.getElementById("paragraph").textContent = `my name is ${firstName} ${lastName} and i study at ${school}`
+
+
+var Title = "The Alchemist"
+var Author = " Paulo Coelho"
+var Published = "1988"
+var Available = "True"
+
+document.getElementById ("Title").textContent = `Title: ${Title}`
+document.getElementById ("Author").textContent = `Author: ${Author}`
+document.getElementById ("Published").textContent = `Published: ${Published}`
+document.getElementById ("Available").textContent = `Available: ${Available}`
+
+
+let username = "username"
+let email = "email"
+let phonenumber = "phonenumber"
+let isVerified = "isverified"
+
+console.log(`User ${username} can be contacted via ${email} or ${phonenumber} Verified: ${isVerified}`)
+
+
+
